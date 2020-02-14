e701106dcb9aeb926ae212fe3451d42f307c85fe
FLAGEUL Cedric <Cdric Flageul>
2020-02-14 09:32:01 +0100

    Add redirection to logfile missing in previous commit.

--- src/include/Module/ModuleCoefficientRepartition.F90
+++ src/include/Module/ModuleCoefficientRepartition.F90
@@ -213,7 +213,7 @@ contains
        allocate(repartition_coefficient(N_size))
       !OPEN(12,file='coeff_reff.txt')
       do i = 1, N_size
-             if (ssh_standalone) write( cTemp,'(i4)' ) (i-1)
+             write( cTemp,'(i4)' ) (i-1)
              Temp=trim(adjustl(cTemp ))
              DIM_NAME = 'Ncoef_'//Temp
               ! Get the varid of the data variable, based on its name.
@@ -363,7 +363,8 @@ contains
              i2=index2_repartition_coefficient(i)%arr(l)
              TempCoef=TempCoef+repartition_coefficient(i)%arr(l)
            enddo
-           if (ssh_standalone) print*,'coef sum',i,TempCoef
+           if (ssh_standalone) write(*,*) 'coef sum',i,TempCoef
+           if (ssh_logger) write(logfile,*) 'coef sum',i,TempCoef
     enddo
     close(11)
 
