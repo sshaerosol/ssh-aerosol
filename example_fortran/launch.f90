d0c0366d6c72895ce755f31bc65ecc7006c2d8c0
Florian Couvidat <Florian.Couvidat@ineris.fr>
2020-03-03 14:23:28 +0100

    Add layer treatment for viscosity in the fortran interface.

--- example_fortran/launch.f90
+++ example_fortran/launch.f90
@@ -1,40 +1,101 @@
+
 PROGRAM main
   USE SSHaerosol
   IMPLICIT NONE
 
-  INTEGER,PARAMETER :: nbins=50
-  INTEGER,PARAMETER :: nspecies=40
+  INTEGER :: nbins
+  INTEGER :: nspecies
+  INTEGER :: naero
+  INTEGER :: itest
+  INTEGER :: nlayers
+  INTEGER :: iemis
   DOUBLE PRECISION :: duration,lat,lon,temp,pres,relh
-  double precision,dimension(nbins) :: number
-  double precision,dimension(nspecies) :: gas_conc
-  double precision,dimension(nbins,nspecies) :: aero_conc
-  character (len=40) :: namelist_ssh
+  double precision,allocatable,dimension(:) :: Vlayers
+  double precision,allocatable,dimension(:) :: number
+  double precision,allocatable,dimension(:) :: gas_conc
+  double precision,allocatable,dimension(:,:) :: aero_conc
+  character (len=40) :: namelist_ssh  
 
-  namelist_ssh="./namelist_modssh.ssh" 
-  
-  lat=0.d0
-  lon=0.d0
-  
-  duration=3600.d0
-  pres=1.01325e05
-  temp=288.15d0
-  relh=0.6d0  
+  itest=1
 
-  number=0.d0
-  gas_conc=0.d0
-  aero_conc=0.d0
-  gas_conc(5)=8.0d0
-  aero_conc(:,4)=(/7.222506E-19, 1.191796E-17, 1.783646E-16, 2.421107E-15, 2.980732E-14, 3.328443E-13, 3.371117E-12, 3.096894E-11, 2.580500E-10, 1.950345E-09, 1.337071E-08, 8.314528E-08, 4.689924E-07, 2.399630E-06, 1.113844E-05, 4.714408E-05, 1.987428E-04, 1.190799E-03, 8.254623E-03, 3.246224E-02, 5.914383E-02, 6.289222E-02, 7.917661E-02, 1.392658E-01, 2.426590E-01, 3.854985E-01, 5.556413E-01, 7.265657E-01, 8.619621E-01, 9.278976E-01, 9.067632E-01, 8.053857E-01, 6.525903E-01, 4.879282E-01, 3.484007E-01, 2.601101E-01, 2.374289E-01, 2.873215E-01, 4.142062E-01, 6.217591E-01, 9.106726E-01, 1.273746E+00, 1.690945E+00, 2.127184E+00, 2.534756E+00, 2.860762E+00, 3.057965E+00, 3.095896E+00, 2.968541E+00, 2.695891E+00/)
-  aero_conc(:,5)=(/3.095360E-19, 5.107696E-18, 7.644198E-17, 1.037617E-15, 1.277457E-14, 1.426476E-13, 1.444764E-12, 1.327240E-11, 1.105928E-10, 8.358622E-10, 5.730304E-09, 3.563369E-08, 2.009968E-07, 1.028413E-06, 4.773617E-06, 2.020461E-05, 8.517548E-05, 5.103422E-04, 3.537695E-03, 1.391239E-02, 2.534736E-02, 2.695381E-02, 3.393283E-02, 5.968534E-02, 1.039967E-01, 1.652136E-01, 2.381320E-01, 3.113853E-01, 3.694123E-01, 3.976704E-01, 3.886128E-01, 3.451653E-01, 2.796816E-01, 2.091121E-01, 1.493146E-01, 1.114758E-01, 1.017553E-01, 1.231378E-01, 1.775169E-01, 2.664682E-01, 3.902882E-01, 5.458911E-01, 7.246907E-01, 9.116503E-01, 1.086324E+00, 1.226041E+00, 1.310556E+00, 1.326812E+00, 1.272232E+00, 1.155382E+00/)
+  if (itest==0) then
+     ! Nucleation example
+     print*,"Nucleation test case"    
 
-  !To create the coefficient repartition file
-  call initialisation_repart_coeff(namelist_ssh)
+     namelist_ssh="./namelist_modssh.ssh"     
 
-  !Launch SSH
-  call launch_ssh_aerosolonly(namelist_ssh,nbins,nspecies,duration,lat,lon,temp,pres,relh,gas_conc,aero_conc,number)  
+     duration=3600.d0
+     pres=1.01325e05
+     temp=288.15d0
+     relh=0.6d0
 
-  print*,number
-  print*,gas_conc(5)
-  print*,aero_conc(:,5)
+     iemis=1
+
+     call initialisation_modssh(namelist_ssh,iemis,naero,nspecies,nbins,nlayers,Vlayers)
+     print*,naero,nspecies,nbins,nlayers
+     print*,Vlayers
+
+     allocate(number(nbins))
+     allocate(gas_conc(nspecies))
+     allocate(aero_conc(nbins,naero))
+
+     number=0.d0
+     gas_conc=0.d0
+     aero_conc=0.d0
+     gas_conc(5)=8.0d0
+     aero_conc(:,4)=(/7.222506E-19, 1.191796E-17, 1.783646E-16, 2.421107E-15, 2.980732E-14, 3.328443E-13, 3.371117E-12, 3.096894E-11, 2.580500E-10, 1.950345E-09, 1.337071E-08, 8.314528E-08, 4.689924E-07, 2.399630E-06, 1.113844E-05, 4.714408E-05, 1.987428E-04, 1.190799E-03, 8.254623E-03, 3.246224E-02, 5.914383E-02, 6.289222E-02, 7.917661E-02, 1.392658E-01, 2.426590E-01, 3.854985E-01, 5.556413E-01, 7.265657E-01, 8.619621E-01, 9.278976E-01, 9.067632E-01, 8.053857E-01, 6.525903E-01, 4.879282E-01, 3.484007E-01, 2.601101E-01, 2.374289E-01, 2.873215E-01, 4.142062E-01, 6.217591E-01, 9.106726E-01, 1.273746E+00, 1.690945E+00, 2.127184E+00, 2.534756E+00, 2.860762E+00, 3.057965E+00, 3.095896E+00, 2.968541E+00, 2.695891E+00/)
+     aero_conc(:,5)=(/3.095360E-19, 5.107696E-18, 7.644198E-17, 1.037617E-15, 1.277457E-14, 1.426476E-13, 1.444764E-12, 1.327240E-11, 1.105928E-10, 8.358622E-10, 5.730304E-09, 3.563369E-08, 2.009968E-07, 1.028413E-06, 4.773617E-06, 2.020461E-05, 8.517548E-05, 5.103422E-04, 3.537695E-03, 1.391239E-02, 2.534736E-02, 2.695381E-02, 3.393283E-02, 5.968534E-02, 1.039967E-01, 1.652136E-01, 2.381320E-01, 3.113853E-01, 3.694123E-01, 3.976704E-01, 3.886128E-01, 3.451653E-01, 2.796816E-01, 2.091121E-01, 1.493146E-01, 1.114758E-01, 1.017553E-01, 1.231378E-01, 1.775169E-01, 2.664682E-01, 3.902882E-01, 5.458911E-01, 7.246907E-01, 9.116503E-01, 1.086324E+00, 1.226041E+00, 1.310556E+00, 1.326812E+00, 1.272232E+00, 1.155382E+00/)
+
+     !To create the coefficient repartition file
+     ! call initialisation_repart_coeff(namelist_ssh)
+
+     !Launch SSH
+     print*,"Launch SSH-aerosol for nucleation test case"
+     call launch_ssh_aerosolonly(namelist_ssh,iemis,naero,nbins,nspecies,duration,temp,pres,relh,gas_conc,aero_conc,number)
+     print*,"SSH-aerosol suceeded"
+     !call launch_ssh_aerosolonly(namelist_ssh,iemis,naero,nbins,nspecies,duration,temp,pres,relh,gas_conc,aero_conc,number)
+     !print*,"C"
+
+     print*,"Number concentrations: ",number
+     print*,"Ammoniac concentration: ",gas_conc(5)
+     print*,"Ammonium: ",aero_conc(:,5)
+
+  else ! Visco 5 POAmP test case     
+     
+     namelist_ssh="./namelist_modssh_visco.ssh"
+     
+     duration=2419200.0d0 !3600.d0
+     pres=1.01325e05
+     temp=298.0
+     relh=0.1d0
+
+     iemis=0
+     
+     call initialisation_modssh(namelist_ssh,iemis,naero,nspecies,nbins,nlayers,Vlayers)
+     print*,naero,nspecies,nbins,nlayers
+     print*,Vlayers
+         
+     allocate(number(nbins))
+     allocate(gas_conc(nspecies))
+     allocate(aero_conc(nbins,naero))
+
+     number=0.d0
+     gas_conc=0.d0
+     aero_conc=0.d0
+     
+     gas_conc(29)=5.0d0
+     gas_conc(25)=0.01d0
+     aero_conc(1,93:97)=5.0d0*Vlayers(:)
+     print*,aero_conc(1,93:97)
+
+     print*,"Launch SSH-aerosol for nucleation test case"
+     call launch_ssh_aerosolonly(namelist_ssh,iemis,naero,nbins,nspecies,duration,temp,pres,relh,gas_conc,aero_conc,number)
+     print*,"SSH-aerosol suceeded"
+
+     print*,"POAmP gas concentration: ",gas_conc(29)
+     print*,"POAmP aerosol concentration: ",sum(aero_conc(1,113:117))
+     print*,"SOAlP aerosol concentration: ",sum(aero_conc(1,93:97))
+     print*,"Number: ",number
+  endif
   
 END PROGRAM main
--- src/ModuleSSH-aerosol.f90
+++ src/ModuleSSH-aerosol.f90
@@ -8,7 +8,7 @@ module SSHaerosol
 
 contains
 
-  subroutine initialisation_repart_coeff(namelist_ssh)
+  subroutine initialisation_modssh(namelist_ssh,iemis,naero,nspecies,nbins,nlayers,Vlayers)
 
     use aInitialization
     use jAdaptstep
@@ -18,18 +18,21 @@ contains
     use lDiscretization
     use Resultoutput
     use gCoagulation
-    use mod_photolysis
+    use mod_photolysis   
 
     implicit none
 
     character (len=40) :: namelist_ssh  ! Configuration file
+    integer naero,nspecies,nbins,iemis,nlayers
+    double precision,allocatable,dimension(:) :: Vlayers
+    
     ssh_standalone = .false.
 
     call dimensions(N_gas, n_reaction, n_photolysis)  
 
     call read_namelist(namelist_ssh)                  
 
-    tag_emis=0
+    tag_emis=iemis
     i_compute_repart = 1
     i_write_repart = 0
 
@@ -39,150 +42,159 @@ contains
 
     call init_distributions()
 
+    naero=N_aerosol_layers
+    nspecies=N_aerosol
+    nbins=N_sizebin
+    nlayers=nlayer
+    allocate(Vlayers(nlayer))
+    Vlayers=Vlayer
+
     !call free_allocated_memory()
     !IF (with_coag.EQ.1) call DeallocateCoefficientRepartition()
-  end subroutine initialisation_repart_coeff
+  end subroutine initialisation_modssh
 
-  subroutine launch_ssh_chemonly(namelist_ssh,ngas,nbins,lat,lon,duration,temp,pres,relh,atte,gas,gas_conc,aero_conc,number)
-    use aInitialization
-    use jAdaptstep
-    use bCoefficientRepartition
-    use mEmissions
-    use netcdf
-    use lDiscretization
-    use Resultoutput
-    use gCoagulation
-    use mod_photolysis
+!!$  subroutine launch_ssh_chemonly(namelist_ssh,ngas,nbins,lat,lon,duration,temp,pres,relh,atte,gas,gas_conc,aero_conc,number)
+!!$    use aInitialization
+!!$    use jAdaptstep
+!!$    use bCoefficientRepartition
+!!$    use mEmissions
+!!$    use netcdf
+!!$    use lDiscretization
+!!$    use Resultoutput
+!!$    use gCoagulation
+!!$    use mod_photolysis
+!!$
+!!$    implicit none
+!!$
+!!$    integer :: t, j, k, s, js,jesp,day,nbins,nspecies,ngas
+!!$    character (len=40) :: namelist_ssh  ! Configuration file
+!!$    double precision :: duration,temp,pres,relh,atte
+!!$    double precision :: ttmassaero = 0.d0, ttmass = 0.d0, totsulf = 0.d0
+!!$    double precision,dimension(nbins) :: number
+!!$    double precision,dimension(nspecies) :: gas_conc
+!!$    double precision,dimension(nbins,nspecies) :: aero_conc
+!!$
+!!$    double precision :: t_since_update_photolysis                                                                                    
+!!$
+!!$    ! Read the number of gas-phase species and chemical reactions
+!!$    call dimensions(N_gas, n_reaction, n_photolysis)  
+!!$
+!!$    call read_namelist(namelist_ssh)
+!!$
+!!$    if (.not.allocated(init_bin_number)) call read_namelist(namelist_ssh)   
+!!$
+!!$    tag_emis=0
+!!$    i_compute_repart = 1
+!!$    i_write_repart = 0
+!!$
+!!$    if (.not.allocated(molecular_weight)) call read_inputs() 
+!!$    Temperature=temp
+!!$    Pressure=pres   
+!!$    Relative_Humidity=relh
+!!$    initial_time=0.d0
+!!$    final_time=duration
+!!$    nt = int((final_time-initial_time) / delta_t)
+!!$    pressure_sat = 611.2d0* dexp(17.67d0* (temperature - 273.15d0) / (temperature - 29.65d0))
+!!$    Relative_Humidity = DMIN1(DMAX1(Relative_Humidity, Threshold_RH_inf), Threshold_RH_sup)
+!!$    humidity =  1.d0/(Pressure/(pressure_sat *0.62197d0* Relative_Humidity)-1.d0)
+!!$    attenuation=atte
+!!$    concentration_gas_all=0.d0
+!!$    init_bin_mass=0.d0
+!!$    init_mass=0.d0                    
+!!$
+!!$    call init_parameters()  
+!!$
+!!$    call init_distributions()  
+!!$
+!!$    if ((tag_chem .ne. 0).AND.(option_photolysis.eq.2)) then
+!!$       ! Allocate arrays for photolysis
+!!$       call allocate_photolysis()    
+!!$       ! Read photolysis rate for current day
+!!$       current_time = initial_time
+!!$       call init_photolysis() 
+!!$       call interpol_photolysis()
+!!$    endif
+!!$    ! **** simulation starts 
+!!$    t_since_update_photolysis = 0.d0
+!!$
+!!$    do t = 1, nt
+!!$
+!!$       current_time = initial_time + (t - 1) * delta_t
+!!$       t_since_update_photolysis = t_since_update_photolysis +  delta_t
+!!$
+!!$       if (ssh_standalone) write(*,*) "Performing iteration #" // trim(str(t)) // "/" // trim(str(nt))
+!!$       if (ssh_logger) write(logfile,*) "Performing iteration #" // trim(str(t)) // "/" // trim(str(nt))
+!!$
+!!$       ! Read the photolysis rates.
+!!$       if ((tag_chem .ne. 0).AND.(option_photolysis.eq.2)) then
+!!$          if (t_since_update_photolysis >= time_update_photolysis) then
+!!$             call interpol_photolysis()
+!!$             t_since_update_photolysis = 0.d0
+!!$          endif
+!!$       endif
+!!$
+!!$       ! Emissions
+!!$       if (tag_emis .ne. 0) call emission(delta_t)
+!!$
+!!$       ! Gas-phase chemistry
+!!$
+!!$       ! initial and final physical parameters are set same
+!!$       ! no volumetric emission
+!!$       ! 0 : vertical gas volumetric emission    1 : with number 
+!!$       ! 0 : not take into account cloud    0.d0 : air water content fracion sets to 0  
+!!$
+!!$       if (tag_chem .ne. 0) then
+!!$          call chem(n_gas, n_reaction, n_photolysis, photolysis_reaction_index,&
+!!$               ns_source, source_index, conversionfactor, conversionfactorjacobian,&
+!!$               0, lwc_cloud_threshold, molecular_weight, &
+!!$               current_time, attenuation, &
+!!$               humidity, temperature,&
+!!$               pressure, source, &
+!!$               photolysis_rate, delta_t, attenuation,&
+!!$               humidity, temperature,&
+!!$               pressure, source, &
+!!$               photolysis_rate, longitude,&
+!!$               latitude, concentration_gas_all,&
+!!$               0, with_heterogeneous, n_aerosol, n_size, n_fracmax,&
+!!$               0.d0,&
+!!$               diam_bound, fixed_density, &
+!!$               wet_diameter, &
+!!$               heterogeneous_reaction_index, &
+!!$               concentration_mass,&
+!!$               with_adaptive, adaptive_time_step_tolerance,&
+!!$               min_adaptive_time_step, option_photolysis, ind_jbiper, ind_kbiper,&
+!!$               1, not(with_fixed_density), concentration_number, &
+!!$               mass_density)
+!!$       end if
+!!$
+!!$       ! re-calculate total_mass(N_aerosol) because mass change due to gas-phase chemistry  
+!!$       total_aero_mass = 0.d0
+!!$       total_mass = 0.d0
+!!$       do s = 1, N_aerosol_layers
+!!$          jesp = List_species(s)
+!!$          do j=1,N_size
+!!$             total_aero_mass(jesp) = total_aero_mass(jesp) + concentration_mass(j,s)
+!!$          enddo
+!!$       enddo
+!!$       ! update mass conc. of aerosol precursors
+!!$       ! concentration_gas_all(precursor_index) -> concentration_gas(n_aerosol)
+!!$       do s = 1, N_aerosol
+!!$          if (aerosol_species_interact(s) .gt. 0) then
+!!$             concentration_gas(s) = concentration_gas_all(aerosol_species_interact(s))
+!!$          end if
+!!$          total_mass(s) = total_mass(s) + concentration_gas(s) + total_aero_mass(s)
+!!$       end do
+!!$    end do			! finsh simulation
+!!$
+!!$  end subroutine launch_ssh_chemonly
 
-    implicit none
-
-    integer :: t, j, k, s, js,jesp,day,nbins,nspecies
-    character (len=40) :: namelist_ssh  ! Configuration file
-    double precision :: duration,temp,pres,relh
-    double precision :: ttmassaero = 0.d0, ttmass = 0.d0, totsulf = 0.d0
-    double precision,dimension(nbins) :: number
-    double precision,dimension(nspecies) :: gas_conc
-    double precision,dimension(nbins,nspecies) :: aero_conc
-    double precision :: ttmassaero = 0.d0, ttmass = 0.d0, totsulf = 0.d0
-
-    double precision :: t_since_update_photolysis                                                                                    
-
-    ! Read the number of gas-phase species and chemical reactions
-    call dimensions(N_gas, n_reaction, n_photolysis)  
-
-    call read_namelist(namelist_ssh)
-
-    if (.not.allocated(init_bin_number)) call read_namelist(namelist_ssh)   
-
-    tag_emis=0
-    i_compute_repart = 1
-    i_write_repart = 0
-
-    if (.not.allocated(molecular_weight)) call read_inputs() 
-    Temperature=temp
-    Pressure=pres   
-    Relative_Humidity=relh
-    initial_time=0.d0
-    final_time=duration
-    nt = int((final_time-initial_time) / delta_t)
-    pressure_sat = 611.2d0* dexp(17.67d0* (temperature - 273.15d0) / (temperature - 29.65d0))
-    Relative_Humidity = DMIN1(DMAX1(Relative_Humidity, Threshold_RH_inf), Threshold_RH_sup)
-    humidity =  1.d0/(Pressure/(pressure_sat *0.62197d0* Relative_Humidity)-1.d0)
-    attenuation=atte
-    concentration_gas_all=0.d0
-    init_bin_mass=0.d0
-    init_mass=0.d0                    
-
-    call init_parameters()  
-
-    call init_distributions()  
-
-    if ((tag_chem .ne. 0).AND.(option_photolysis.eq.2)) then
-       ! Allocate arrays for photolysis
-       call allocate_photolysis()    
-       ! Read photolysis rate for current day
-       current_time = initial_time
-       call init_photolysis() 
-       call interpol_photolysis()
-    endif
-    ! **** simulation starts 
-    t_since_update_photolysis = 0.d0
-
-    do t = 1, nt
-
-       current_time = initial_time + (t - 1) * delta_t
-       t_since_update_photolysis = t_since_update_photolysis +  delta_t
-
-       if (ssh_standalone) write(*,*) "Performing iteration #" // trim(str(t)) // "/" // trim(str(nt))
-       if (ssh_logger) write(logfile,*) "Performing iteration #" // trim(str(t)) // "/" // trim(str(nt))
-
-       ! Read the photolysis rates.
-       if ((tag_chem .ne. 0).AND.(option_photolysis.eq.2)) then
-          if (t_since_update_photolysis >= time_update_photolysis) then
-             call interpol_photolysis()
-             t_since_update_photolysis = 0.d0
-          endif
-       endif
-
-       ! Emissions
-       if (tag_emis .ne. 0) call emission(delta_t)
-
-       ! Gas-phase chemistry
-
-       ! initial and final physical parameters are set same
-       ! no volumetric emission
-       ! 0 : vertical gas volumetric emission    1 : with number 
-       ! 0 : not take into account cloud    0.d0 : air water content fracion sets to 0  
-
-       if (tag_chem .ne. 0) then
-          call chem(n_gas, n_reaction, n_photolysis, photolysis_reaction_index,&
-               ns_source, source_index, conversionfactor, conversionfactorjacobian,&
-               0, lwc_cloud_threshold, molecular_weight, &
-               current_time, attenuation, &
-               humidity, temperature,&
-               pressure, source, &
-               photolysis_rate, delta_t, attenuation,&
-               humidity, temperature,&
-               pressure, source, &
-               photolysis_rate, longitude,&
-               latitude, concentration_gas_all,&
-               0, with_heterogeneous, n_aerosol, n_size, n_fracmax,&
-               0.d0,&
-               diam_bound, fixed_density, &
-               wet_diameter, &
-               heterogeneous_reaction_index, &
-               concentration_mass,&
-               with_adaptive, adaptive_time_step_tolerance,&
-               min_adaptive_time_step, option_photolysis, ind_jbiper, ind_kbiper,&
-               1, not(with_fixed_density), concentration_number, &
-               mass_density)
-       end if
-
-       ! re-calculate total_mass(N_aerosol) because mass change due to gas-phase chemistry  
-       total_aero_mass = 0.d0
-       total_mass = 0.d0
-       do s = 1, N_aerosol_layers
-          jesp = List_species(s)
-          do j=1,N_size
-             total_aero_mass(jesp) = total_aero_mass(jesp) + concentration_mass(j,s)
-          enddo
-       enddo
-       ! update mass conc. of aerosol precursors
-       ! concentration_gas_all(precursor_index) -> concentration_gas(n_aerosol)
-       do s = 1, N_aerosol
-          if (aerosol_species_interact(s) .gt. 0) then
-             concentration_gas(s) = concentration_gas_all(aerosol_species_interact(s))
-          end if
-          total_mass(s) = total_mass(s) + concentration_gas(s) + total_aero_mass(s)
-       end do
-    end do			! finsh simulation
-
-  end subroutine launch_ssh_chemonly
-
-  subroutine launch_ssh_aerosolonly(namelist_ssh,nbins,nspecies,duration,temp,pres,relh,gas_conc,aero_conc,number)
+  subroutine launch_ssh_aerosolonly(namelist_ssh,iemis,naero,nbins,nspecies,duration,temp,pres,relh,gas_conc,aero_conc,number)    
     ! Computes aerosol formation
     !    With internal mixing
-    !    Inviscid organic aerosol (only one layer)
+    !    Inviscid organic aerosol (only one layer) in that case naero=nspecies=number of aerosol species
+    !    Viscous organic aerosol, 1 species for each organic aerosol layers
+
+    !iemis=1 ; to use emissions. For test cases only
 
     use aInitialization
     use jAdaptstep
@@ -196,13 +208,13 @@ contains
 
     implicit none
 
-    integer :: t, j, k, s, js,jesp,day,nbins,nspecies
+    integer :: t, j, k, s, js,jesp,day,nbins,nspecies,iemis,naero
     character (len=40) :: namelist_ssh  ! Configuration file
     double precision :: duration,temp,pres,relh
     double precision :: ttmassaero = 0.d0, ttmass = 0.d0, totsulf = 0.d0
     double precision,dimension(nbins) :: number
     double precision,dimension(nspecies) :: gas_conc
-    double precision,dimension(nbins,nspecies) :: aero_conc
+    double precision,dimension(nbins,naero) :: aero_conc
 
     double precision :: t_since_update_photolysis
 
@@ -214,14 +226,19 @@ contains
     ! Read the number of gas-phase species and chemical reactions    
     call dimensions(N_gas, n_reaction, n_photolysis)    
 
-    if (.not.allocated(init_bin_number)) call read_namelist(namelist_ssh)   
+    if (.not.allocated(init_bin_number)) call read_namelist(namelist_ssh)    
 
-    tag_emis=0
+    tag_emis=iemis
     i_compute_repart = 1
     i_write_repart = 0
 
     if (.not.allocated(molecular_weight)) call read_inputs()    
 
+    if (N_aerosol_layers.ne.naero) then
+       print*,"Number of aerosol species (including layers) should be equal to : ",N_aerosol_layers
+       stop
+    endif
+    
     if (N_aerosol.ne.nspecies) then
        print*,"Number of aerosol species should be equal to : ",N_aerosol
        stop
@@ -250,23 +267,38 @@ contains
        if (aerosol_species_interact(js)>0) then
           concentration_gas_all(aerosol_species_interact(js))=concentration_gas(js)
        endif
-       do k=1, N_sizebin
-          init_bin_mass(k,js) = aero_conc(k,js)
-          init_mass(js)=init_mass(js) + aero_conc(k,js)
+    enddo
+
+    do s = 1, N_aerosol_layers
+       jesp = List_species(s)
+       do j=1,N_size
+          init_bin_mass(j,jesp) = init_bin_mass(j,jesp) + concentration_mass(j,s)
+          init_mass(jesp)=init_mass(jesp) + aero_conc(j,js)
        enddo
     enddo
-    aero_total_mass=sum(init_mass(1:N_aerosol)) 
-
+    aero_total_mass=sum(aero_conc(:,1:N_aerosol_layers))
+    
     do k=1,N_sizebin
        init_bin_number(k)=number(k)
     enddo
+   
+    call init_parameters()
+        
+    call init_distributions()   
 
-    call init_parameters()   
-
-    call init_distributions()    
-
+    do s = 1, N_aerosol_layers
+       jesp = List_species(s)
+       do j=1,N_size
+          concentration_mass(j,s)=aero_conc(j,s)
+       enddo
+    enddo
+    
     do t = 1, nt
-       ! re-calculate total_mass(N_aerosol) because mass change due to gas-phase chemistry  
+       print*,t,sum(concentration_mass(1,93:97))
+       
+       ! Emissions
+       if (tag_emis .ne. 0) call emission(delta_t)
+       
        total_aero_mass = 0.d0
        total_mass = 0.d0
        do s = 1, N_aerosol_layers
@@ -299,6 +331,8 @@ contains
 
     do s = 1, N_aerosol
        gas_conc(s)=concentration_gas(s)
+    enddo
+    do s = 1, N_aerosol_layers
        aero_conc(:,s)=concentration_mass(:,s)
     enddo
     number(:)=concentration_number(:)
