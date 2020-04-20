23ffc4b2c3f240f159680f95d4b1ba4b5a0f210d
Karine Sartelet <sartelet@cerea.enpc.fr>
2020-04-20 21:51:52 +0200

    Add test case with cutoff diameter between dynamic and equilibrium size section for condensation/evaporation. Improve graphs.

--- graph/gas_cond-evap.py
+++ graph/gas_cond-evap.py
@@ -15,18 +15,24 @@ plt.ioff()
 ################### input parameters #################################"
 pcase_dyn = '../results/cond-evap-inorg/cond-evap-dyn'
 pcase_eq = '../results/cond-evap-inorg/cond-evap-eq'
+pcase_icut = '../results/cond-evap-inorg/cond-evap-icut'
 tag_fig = 'time'
 sizebin_ssh = 15
 initial_time = 0.0        
 final_time = 1000.0             
 delta_t = 10.0                 
 #####################################################################
+font = {'size'   : 14}
+matplotlib.rc('font', **font)
+matplotlib.pyplot.xticks(fontsize=14)
 
 nt = int((final_time-initial_time)/delta_t)
 conc_nh3_dyn = np.zeros(nt)
 conc_nh3_eq = np.zeros(nt)
+conc_nh3_icut = np.zeros(nt)
 conc_hno3_dyn = np.zeros(nt)
 conc_hno3_eq = np.zeros(nt)
+conc_hno3_icut = np.zeros(nt)
 ssh_time = np.zeros(nt)
 for i in range(nt):
    ssh_time[i] = initial_time + delta_t * i 
@@ -41,16 +47,20 @@ with open (pcase_eq+'/gas/NH3.txt') as finit :
 	values = finit.read().splitlines()
 	for i in range(nt):
            conc_nh3_eq[i] = float(values[i])
+with open (pcase_icut+'/gas/NH3.txt') as finit :
+	values = finit.read().splitlines()
+	for i in range(nt):
+           conc_nh3_icut[i] = float(values[i])
 plt.plot(ssh_time, conc_nh3_dyn, label = 'SSH dyn')
 plt.plot(ssh_time, conc_nh3_eq, label = 'SSH eq')
+plt.plot(ssh_time, conc_nh3_icut, label = 'SSH hyb')
 
 plt.xlabel(r'time (s)')
-plt.title( 'NH3 time evolution')
+plt.title( 'NH$_3$ time evolution')
 plt.legend(loc ='best')		# show legend
 fig.savefig('NH3_COND-EVAP_'+tag_fig)
 
-
-fig = plt.figure(2,figsize = (15,15))
+fig.clf()
 with open (pcase_dyn+'/gas/HNO3.txt') as finit :
 	values = finit.read().splitlines()
 	for i in range(nt):
@@ -59,10 +69,16 @@ with open (pcase_eq+'/gas/HNO3.txt') as finit :
 	values = finit.read().splitlines()
 	for i in range(nt):
            conc_hno3_eq[i] = float(values[i])
+with open (pcase_icut+'/gas/HNO3.txt') as finit :
+	values = finit.read().splitlines()
+	for i in range(nt):
+           conc_hno3_icut[i] = float(values[i])
 plt.plot(ssh_time, conc_hno3_dyn, label = 'SSH dyn')
 plt.plot(ssh_time, conc_hno3_eq, label = 'SSH eq')
+plt.plot(ssh_time, conc_hno3_icut, label = 'SSH hyb')
 
 plt.xlabel(r'time (s)')
-plt.title( 'HNO3 time evolution')
+plt.title( 'HNO$_3$ time evolution')
 plt.legend(loc ='best')		# show legend
+plt.tight_layout()
 fig.savefig('HNO3_COND-EVAP_'+tag_fig)
