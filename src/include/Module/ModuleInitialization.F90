5ac19bf80e320adca291969fc05d9b9f7e2ac91c
Karine Sartelet <sartelet@cerea.enpc.fr>
2020-11-19 10:18:25 +0100

    Force phases to not be coupled when organics are at thermodynamic equilbrium.

--- src/include/Module/ModuleInitialization.F90
+++ src/include/Module/ModuleInitialization.F90
@@ -1102,6 +1102,11 @@ contains
        end if
        if(coupled_phases == 1) then
           i_hydrophilic = 1 
+          if(ISOAPDYN == 0) then
+            if (ssh_standalone) write(*,*) 'Phases can not be coupled if ISOAPDYN = 0'
+            if (ssh_logger) write(logfile,*) 'Phases can not be coupled if ISOADYN = 0'
+            i_hydrophilic = 0
+          endif
        else 
           i_hydrophilic = 0 
        endif
