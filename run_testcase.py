6f9c103ca027f3a9469263eb09431d7dd8bbbe22
Youngseob Kim <youngseob.kim@enpc.fr>
2021-07-01 10:06:54 +0200

    Changed default program from python2 to python3. And added new arguments arg_run and arg_figure to separate run and generation of figures into two steps.

--- run_testcase.py
+++ run_testcase.py
@@ -15,6 +15,9 @@ import subprocess
 # Authors: Zhizhao Wang and Youngseob Kim
 #########
 
+arg_run = False
+arg_figure = True
+
 def multiprocessing_func(k):
         if k[-4:] == '.ssh':
 	        print("Run: namelist file : " + k)
@@ -28,40 +31,42 @@ if __name__ == '__main__':
         ssh_dir = './'
         os.chdir(ssh_dir)
 
-        # remove all result files.
-        subprocess.run(['rm', '-rf', 'results'])
+        if (arg_run):
+            # remove all result files.
+            subprocess.run(['rm', '-rf', 'results'])
 
-        # get all file names in the folder INIT
-        all_files = False
-        if all_files:
-            initfile = os.listdir(ssh_dir+ 'INIT/')
-        else:
-            keyword = 'soalp'
-            initfile = [f for f in os.listdir(ssh_dir+ 'INIT/') if re.search(keyword,f)]
+            # get all file names in the folder INIT
+            all_files = True
+            if all_files:
+                initfile = os.listdir(ssh_dir+ 'INIT/')
+            else:
+                keyword = 'soalp'
+                initfile = [f for f in os.listdir(ssh_dir+ 'INIT/') if re.search(keyword,f)]
             
-        for i in range(len(initfile)):
+            for i in range(len(initfile)):
                 p = multiprocessing.Process(target = multiprocessing_func, args = (initfile[i],))
                 processes.append(p)
                 p.start()
 
-        for process in processes:
+            for process in processes:
                 process.join()
 
-        print('That took {} seconds'.format(time.time() - starttime))
+            print('That took {} seconds'.format(time.time() - starttime))
 
-        print('! ! All simulations are done, ')
+            print('! ! All simulations are done, ')
 
-        print('! Run all postprocessing python scripts in graph folder: ')
+            print('! Run all postprocessing python scripts in graph folder: ')
 
-        # record the number of python scripts
-        num = 0
-        # get all file names in the folder graph
-        graphfile =  os.listdir(ssh_dir+ 'graph/')
-        os.chdir('graph')
-        for k in graphfile :
-	        if k[-3:] == '.py':
+        if (arg_figure):
+            # record the number of python scripts
+            num = 0
+            # get all file names in the folder graph
+            graphfile =  os.listdir(ssh_dir+ 'graph/')
+            os.chdir('graph')
+            for k in graphfile :
+	            if k[-3:] == '.py':
 	                num = num + 1
 	                print("Run "+str(num) + ", python script : " + k)
                         # run python script
-	                os.system('python ' + k)
-        print('! ! All processing file are done, ' + 'number of .py files : ' + str(num))
+	                os.system('python3 ' + k)
+            print('! ! All processing file are done, ' + 'number of .py files : ' + str(num))
