36bc414e700ba950d6aadbcb7f79401b82f3c505
Youngseob Kim <youngseob.kim@enpc.fr>
2022-03-23 12:00:56 +0100

    Minor correction.

--- run_testcase.py
+++ run_testcase.py
@@ -10,19 +10,22 @@ import subprocess
 # Description :
 # 	run all namelist.ssh files and all pythonscript for graphs
 #
-# Last update : 05/07/2019
+# Last update : 15/03/2022
 #
 # Authors: Zhizhao Wang and Youngseob Kim
 #########
 
-arg_run = False
+arg_run = True
 arg_figure = True
+multiproc_run = True
 
 def multiprocessing_func(k):
         if k[-4:] == '.ssh':
-	        print("Run: namelist file : " + k)
-                # run simulations
-	        os.system('time ./ssh-aerosol INIT/' + k )
+            print("Run: namelist file : " + k)
+            # run simulations
+            cmd = "time ./ssh-aerosol INIT/" + k + \
+            " > results/ssh_log_" + k[:-4] + " 2>&1"
+            os.system(cmd)
 
 if __name__ == '__main__':
         starttime = time.time()
@@ -31,25 +34,35 @@ if __name__ == '__main__':
         ssh_dir = './'
         os.chdir(ssh_dir)
 
+        if (os.path.isfile('./ssh-aerosol') == False):
+            print("IOError: did you compile the program?")
+            sys.exit()
+        
         if (arg_run):
             # remove all result files.
-            subprocess.run(['rm', '-rf', 'results'])
+            subprocess.run(['rm', '-rf', 'results/*'])
 
             # get all file names in the folder INIT
             all_files = True
             if all_files:
                 initfile = os.listdir(ssh_dir+ 'INIT/')
             else:
-                keyword = 'soalp'
+                keyword = 'monomer' # 'soalp'
                 initfile = [f for f in os.listdir(ssh_dir+ 'INIT/') if re.search(keyword,f)]
-            
-            for i in range(len(initfile)):
-                p = multiprocessing.Process(target = multiprocessing_func, args = (initfile[i],))
-                processes.append(p)
-                p.start()
 
-            for process in processes:
-                process.join()
+            for i in range(len(initfile)):
+                print("run the case ", initfile[i])
+
+                if (multiproc_run):
+                    p = multiprocessing.Process(target = multiprocessing_func, args = (initfile[i],))
+                    processes.append(p)
+                    p.start()
+                else:
+                    multiprocessing_func(initfile[i])
+
+            if (multiproc_run):                    
+                for process in processes:
+                    process.join()
 
             print('That took {} seconds'.format(time.time() - starttime))
 
