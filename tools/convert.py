f2276d2308c4c42735eebb61b8be3f6b2041b297
zhizhaowa <zhizhaow@ucr.edu>
2024-04-01 19:31:18 -0600

    Update tools/convery.py: now able to convert some SPEC kinetics.

--- tools/convert.py
+++ tools/convert.py
@@ -2,6 +2,10 @@ import json
 import sys
 
 tag_ratio = True # Used only for GENOA output
+invalid_line = '!!!\nKINETIC ARR 0 0 0'
+
+all_keys = ['ARR1', 'ARR2', 'ARR3', 'MCM1', 'MCM2', 'MCM3', 'TROE4', 'TROE5', 'TROE7', 'TROE', 'SPEC']
+
 mcm_keys = {'KRO2NO': '92 1',
             'KRO2HO2':'92 2',
             'KAPHO2':'92 3',
@@ -18,6 +22,17 @@ mcm_keys = {'KRO2NO': '92 1',
             'KBPAN': '93 22',
             'KBPPN': '93 23',
             'KRO2': '93 30'}
+# EXTRA 99 [num] [ratio]
+spec_keys = {
+            '-10': '1',
+            '-11': '2',
+            '-12': '3',
+            '-13': '4',
+            '-14': '5',
+            '-15': '6',
+            '-16': '7',
+            '-17': '8'
+            }
                  
 def check_inputs():
     if len(sys.argv) >= 3:
@@ -56,64 +71,74 @@ def convert_reaction_file(old, new):
                 if 'PHOTOLYSIS' in line:
                     fout.write(line + '\n')
                     continue
+
                 # RO2 pool
                 if 'TB RO2' in line: line = line.replace('TB RO2','RO2 1')
+                
                 # Check keywords
                 parts = [i for i in line.split(' ') if i != '']
-                for key in ['ARR1', 'ARR2', 'ARR3', 'MCM1', 'MCM2', 'MCM3', 'TROE4', 'TROE5', 'TROE7', 'TROE']:
-                    if key in parts:
-                        ind = parts.index(key)
-                        iratio = None
-                        if 'ARR' in key:
-                            parts[ind] = 'ARR'
-                            if key == 'ARR1': parts.extend(['0','0'])
-                            elif key == 'ARR2': parts.insert(ind + 2, '0')
-                        elif 'MCM' in key:
-                            if key == 'MCM3': parts[ind] = 'EXTRA 91'
-                            else:
-                                ikey = lines[i-1].replace('%','').strip()
-                                if '*' in ikey:
-                                    ikey, iratio = ikey.split('*',1)
-                                    print(f'Find ratios and key: {iratio}, {ikey}')
-                                if 'KMT' in ikey:
-                                    val = int(ikey.replace('KMT',''))
-                                    if iratio is None: parts = parts[:ind] + [f"EXTRA 93 {val}"]
-                                    else: parts = parts[:ind] + [f"EXTRA 93 {val} {iratio}"]
-                                elif ikey in mcm_keys: parts = parts[:ind] + [f'EXTRA {mcm_keys[ikey]}']
-                                else: raise ValueError(f'Can not find MCM key {ikey}.')
-                        elif 'TROE' in key:
-                            parts[ind] = 'FALLOFF'
-                            if key in ['TROE4', 'TROE5']:
-                                # Change to negative: (T/300)^-C to (300/T)^C
-                                parts[ind+2] = change_sign(parts[ind+2])
-                                parts[ind+4] = change_sign(parts[ind+4])
-                                # Add missing coefficients
-                                parts.insert(ind+3, '0')
-                                parts.insert(ind+6, '0')
-                                if key == 'TROE4': parts.append('0.6')
-                            else: # For TROE7 and TROE10 (10 never tested yet)
-                                # Change to negative for C3 and C6: (T/300)^-C to (300/T)^C
-                                parts[ind+3] = change_sign(parts[ind+3])
-                                parts[ind+6] = change_sign(parts[ind+6])
-                                # Change orders of C3 and C6
-                                parts[ind+2], parts[ind+3] = parts[ind+3], parts[ind+2]
-                                parts[ind+5], parts[ind+6] = parts[ind+6], parts[ind+5]
-                            # Chage k0 (C1-C3) and kinf locations (C4-C6)
-                            parts[ind+1:ind+4], parts[ind+4:ind+7] = parts[ind+4:ind+7], parts[ind+1:ind+4]
-                            
-                            if key != 'TROE10': parts.append('0 0 0')
-                            else: print('Check TROE10 coefficients order!!!')
-                        else:
-                            print(f'Not processed yet: {line}')
-                            
-                        # Add ratio if need
-                        if tag_ratio and 'ARR' not in key and iratio is None: parts.append('1.0')
-                        break
-                else: # Not find keywords
+                pkeys = [i for i in parts if i in all_keys]
+                if len(pkeys) != 1: # Not find keyword or find multiple keywords
                     n += 1
                     print(f'# Can not convert kinetic rate: {line}, Need to convert manually.')
                     parts[0] = '%'+parts[0]
-                    parts.append('!!!\nKINETIC ARR 0 0 0')
+                    parts.append(invalid_line)
+                else:
+                    # Treat one keyword
+                    key = pkeys[0]
+                    ind = parts.index(key)
+                    iratio = None # default ratio is 1
+                    
+                    if 'ARR' in key:
+                        parts[ind] = 'ARR'
+                        if key == 'ARR1': parts.extend(['0','0'])
+                        elif key == 'ARR2': parts.insert(ind + 2, '0')
+                    elif 'MCM' in key:
+                        if key == 'MCM3': parts[ind] = 'EXTRA 91'
+                        else:
+                            ikey = lines[i-1].replace('%','').strip()
+                            if '*' in ikey:
+                                ikey, iratio = ikey.split('*',1)
+                                print(f'Find ratios and key: {iratio}, {ikey}')
+                            if 'KMT' in ikey:
+                                val = int(ikey.replace('KMT',''))
+                                if iratio is None: parts = parts[:ind] + [f"EXTRA 93 {val}"]
+                                else: parts = parts[:ind] + [f"EXTRA 93 {val} {iratio}"]
+                            elif ikey in mcm_keys: parts = parts[:ind] + [f'EXTRA {mcm_keys[ikey]}']
+                            else: raise ValueError(f'Can not find MCM key {ikey}.')
+                    elif 'TROE' in key:
+                        parts[ind] = 'FALLOFF'
+                        if key in ['TROE4', 'TROE5']:
+                            # Change to negative: (T/300)^-C to (300/T)^C
+                            parts[ind+2] = change_sign(parts[ind+2])
+                            parts[ind+4] = change_sign(parts[ind+4])
+                            # Add missing coefficients
+                            parts.insert(ind+3, '0')
+                            parts.insert(ind+6, '0')
+                            if key == 'TROE4': parts.append('0.6')
+                        else: # For TROE7 and TROE10 (10 never tested yet)
+                            # Change to negative for C3 and C6: (T/300)^-C to (300/T)^C
+                            parts[ind+3] = change_sign(parts[ind+3])
+                            parts[ind+6] = change_sign(parts[ind+6])
+                            # Change orders of C3 and C6
+                            parts[ind+2], parts[ind+3] = parts[ind+3], parts[ind+2]
+                            parts[ind+5], parts[ind+6] = parts[ind+6], parts[ind+5]
+                        # Chage k0 (C1-C3) and kinf locations (C4-C6)
+                        parts[ind+1:ind+3], parts[ind+4:ind+6] = parts[ind+4:ind+6], parts[ind+1:ind+3]
+                        
+                        if key != 'TROE10': parts.append('0 0 0')
+                        else: print('Check TROE10 coefficients order!!!')
+                    elif key == 'SPEC' and len(parts) == ind + 2 and parts[ind+1] in spec_keys:
+                        parts[ind] = 'EXTRA 99'
+                        parts[ind+1] = spec_keys[parts[ind+1]]
+                    else:
+                        n += 1
+                        parts.append(invalid_line)
+                        print(f'Not processed yet: {line}')
+
+                    # Add ratio if need
+                    if tag_ratio and 'ARR' not in key and iratio is None: parts.append('1.0')
+
                 # Write
                 fout.write(' '.join(parts) + '\n')
             else: fout.write(line+'\n')
