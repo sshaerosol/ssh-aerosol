ba3e1ccbdf23e888c1e10929d8d5ec4d03912c0d
Youngseob Kim <youngseob.kim@enpc.fr>
2024-09-13 16:27:29 +0200

    Updated the script run_testcase.py

--- run_testcase.py
+++ run_testcase.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 import os,sys
 import numpy as np
 import time
 import multiprocessing
 import re
 import subprocess
+import logging
 
 #########
 #
@@ -15,13 +18,34 @@ import subprocess
 # Authors: Zhizhao Wang and Youngseob Kim
 #########
 
+# == User configuration
 arg_run = True
 arg_figure = True
 multiproc_run = True
 
+# if use_current_directory = True,
+# input_directory and output_directory are ignored. 
+use_current_directory = True
+input_directory = "/cerea_raid/users/kimy/work/ssh-aerosol/ssh-aerosol.git/"
+output_directory = "/net/libre/merida/kimy/ssh-aerosol-testcase/"
+# ==
+
+"""
+Get date and time as a string
+"""
+def get_now():        
+
+    from datetime import datetime
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d_%H%M%S")
+    return dt_string
+
+
 def multiprocessing_func(k):
         if k[-4:] == '.ssh':
             print("Run: namelist file : " + k)
+            logging.getLogger("Run: namelist file : " + k)
+            
             # run simulations
             cmd = "time ./ssh-aerosol INIT/" + k + \
             " > results/ssh_log_" + k[:-4] + " 2>&1"
@@ -31,11 +55,47 @@ if __name__ == '__main__':
         starttime = time.time()
         processes = []
 
-        ssh_dir = './'
+        if (use_current_directory):
+            simulation_dir = './'
+        else:
+            simulation_time = get_now()
+
+            simulation_dir = output_directory + "/" + simulation_time + "/"
+            os.makedirs(simulation_dir + "/results")
+            os.makedirs(simulation_dir + "/graph")
+
+            # Copy files
+            infile_list = ["ssh-aerosol",
+                           "src",
+                           "INIT",
+                           "coef*",
+                           "graph",
+                           "inputs",
+                           "photolysis",
+                           "species-list"]
+        
+            for infile in infile_list:
+                os.system("cp -Lrf " + input_directory + "/" + 
+                          infile + " " + simulation_dir + "/")
+            
+            print("Make result directory at ", simulation_dir)
+            
+
+        # Make logfile    
+        logging.basicConfig(filename = simulation_dir + '/run_testcase.log',
+                            level = logging.DEBUG,
+                            format='%(asctime)s %(message)s',
+                            datefmt='%d/%m/%Y %H:%M:%S')
+        console = logging.StreamHandler()
+        console.setLevel(logging.ERROR)
+        logging.getLogger("").addHandler(console)
+
+        
+        ssh_dir = simulation_dir
         os.chdir(ssh_dir)
 
         if (os.path.isfile('./ssh-aerosol') == False):
-            print("IOError: did you compile the program?")
+            logging.info("IOError: did you compile the program?")
             sys.exit()
         
         if (arg_run):
@@ -52,24 +112,24 @@ if __name__ == '__main__':
 
             for i in range(len(initfile)):
                 if (initfile[i][-4:]==".ssh"):
-                        print("run the case ", initfile[i])
-
-                        if (multiproc_run):
-                                p = multiprocessing.Process(target = multiprocessing_func, args = (initfile[i],))
-                                processes.append(p)
-                                p.start()
-                        else:
-                                multiprocessing_func(initfile[i])
+                    logging.info("run the case " + initfile[i])
+                        
+                    if (multiproc_run):
+                        p = multiprocessing.Process(target = multiprocessing_func, args = (initfile[i],))
+                        processes.append(p)
+                        p.start()
+                    else:
+                        multiprocessing_func(initfile[i])
 
             if (multiproc_run):                    
                 for process in processes:
                     process.join()
 
-            print('That took {} seconds'.format(time.time() - starttime))
+            logging.info('That took {} seconds'.format(time.time() - starttime))
 
-            print('! ! All simulations are done, ')
+            logging.info('! ! All simulations are done, ')
 
-            print('! Run all postprocessing python scripts in graph folder: ')
+            logging.info('! Run all postprocessing python scripts in graph folder: ')
 
         if (arg_figure):
             # record the number of python scripts
@@ -80,7 +140,7 @@ if __name__ == '__main__':
             for k in graphfile :
 	            if k[-3:] == '.py':
 	                num = num + 1
-	                print("Run "+str(num) + ", python script : " + k)
+	                logging.info("Run "+str(num) + ", python script : " + k)
                         # run python script
 	                os.system('python3 ' + k)
-            print('! ! All processing file are done, ' + 'number of .py files : ' + str(num))
+            logging.info('! ! All processing file are done, ' + 'number of .py files : ' + str(num))
